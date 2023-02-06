c2833cb9e51310113be50c00e325e2a0743af59e
prnessy23 <prshaughnessy@gmail.com>
2023-02-05 16:57:50 -0800

    amended jscript file, delted and reinstalled schema and seed after making changes to both

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,6 @@
 DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db;
+
 USE employee_db; 
 
 CREATE TABLE department (
@@ -11,7 +12,7 @@ CREATE TABLE role (
     title VARCHAR(30)NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INTEGER,
-    INDEX dep_ind (department_id),
+    INDEX dep_ind (department_id)
     CONSTRAINT fk_department FOREIGN KEY (department_id)REFERENCES department(id)ON DELETE SET NULL
 );
 CREATE TABLE employee,(
@@ -19,6 +20,6 @@ id INTEGER PRIMARY KEY AUTO_INCREMENT,
 first_name VARCHAR(30) NOT NULL,
 last_name VARCHAR(30)NOT NULL,
 role_id INTEGER,
-INDEX role_ind (role_id),
+INDEX role_ind (role_id)
 CONSTRAINT fk_manager FOREIGN KEY (manager_id),REFERENCES employee(id)ON DELETE SET NULL
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -3,26 +3,30 @@ VALUES
 ('IT'),
 ('Finance & Accounting'),
 ('Sales & Marketing'),
-('Operations');
+('Operations'),
+
+
 
 INSERT INTO role (title, salary, department_id)
 VALUES
-('Salesperson', 80000, 3),
+
 ('Lead Engineer', 150000, 1),
 ('Software Engineer', 120000, 1),
-('Account Manager',160000, 3),
-('Accountant', 125000, 2),
-('Financial Analyst', 150000, 2),
+('Account Manager',160000, 2),
+('Salesperson', 80000, 2),
+('Accountant', 150000, 3),
+('Financial Analyst', 125000, 3),
 ('Legal Team Lead', 250000, 4)
 ('Lawyer', 190000 , 4);
 
-INSERT INTO employee (first_name,last_name,role_id,manager_id)
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
-('Ashley','Rodriguez',2, null),
-('Kevin','Tupik',1,1),
-('Kumal','Singh', 4, null),
-('Malia','Brown',3,3),
-('Dina','Ferrara',6, null),
-('Eric','Minor',5,5),
-('Sarah','Lourd',7,null),
-('Tom','Allen',8,7);
+
+('Kevin','Tupik', 1, 1),
+('Kumal','Singh', 3, null),
+('Malia','Brown', 2 ,2),
+('Ashley','Rodriguez',4, null),
+('Dina','Ferrara',5 ,3 ),
+('Eric','Minor', 7, null),
+('Sarah','Lourd', 6, 4),
+('Tom','Allen', 8, null);
